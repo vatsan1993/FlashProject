8dc51161c7451b293ab41fe7e5d252bcf6d6a1ea
Srivatsan <vatsan1993@gmail.com>
2019-10-18 13:18:57 +0530

    Added a database using flask-sqlalchemy

--- Herezwhat.py
+++ Herezwhat.py
@@ -1,10 +1,41 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+
 from forms import RegistrationForm, LoginForm
 from flask import flash, redirect
-
+from datetime import datetime
 app= Flask(__name__)
 
 app.config['SECRET_KEY']='5a010e6170bd6627c422d94b9a2c1a46'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db' # relative path
+
+db= SQLAlchemy(app)
+class User(db.Model):
+    id= db.Column(db.Integer, primary_key= True)
+    username= db.Column(db.String(20), unique=True, nullable= False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20),  nullable=False, default="default.jpg")
+    password = db.Column(db.String(60), nullable=False)
+    # The "Post" here is the class name
+    # one-many Relationship.
+    posts= db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return  f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+
+class Post(db.Model):
+    id= db.Column(db.Integer, primary_key= True)
+    title= db.Column(db.String(100), nullable=False)
+    date_posted= db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content= db.Column(db.Text, nullable= False)
+    # "user" is table name. so its  lowercase.
+    user_id= db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post'{self.title}', '{self.date_posted}'_"
+
 # Dummy posts
 posts = [
     {
